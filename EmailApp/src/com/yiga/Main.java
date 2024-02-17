df4ad056315a43068d73a9cda2d4890838fa5034
Ntuthuko Simon Yiga <101830715+Ntuthuko-yiga@users.noreply.github.com>
2024-02-17 22:48:00 +0200

    first commit

---
+++ EmailApp/src/com/yiga/Main.java
@@ -0,0 +1,10 @@
+package com.yiga;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Email person1 = new Email("Ntuthuko", "Yiga");
+        System.out.println(person1.showInfo());
+
+    }
+}
